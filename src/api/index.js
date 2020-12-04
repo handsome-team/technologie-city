8ea070fad7780eda38aa0332b2c0b6a8e3e32f17
chenxueliu <786183405@qq.com>
2020-12-04 09:54:21 +0800

    <feat>:最后合并

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,14 @@
-import axios from 'axios'
-const baseURL = 'http://localhost:9700'
-
-
-
+import axios from "axios";
+const baseURL = "http://8.131.83.251:2089";
 
 // 设置一个响应拦截器
 axios.interceptors.response.use(
   res => res.data,
   err => Promise.reject(err)
 );
-const getProductdata = ()=>{
-  return axios.post(`${baseURL}/product`)
-}
+const getProductdata = () => {
+  return axios.post(`${baseURL}/product`);
+};
 const getCompanyData = () => {
   return axios.get(`${baseURL}/company`);
 };
@@ -31,9 +28,16 @@ const getAboutus = () => {
   return axios.get(baseURL + "/aboutus");
 };
 // 留言板
-const takeMessage = (obj) => {
+const takeMessage = obj => {
   // console.log(obj)
-  return axios.post(baseURL+"/message",{params:obj})
-}
-export { getCompanyData, getCompanyDetail, getConnetData,takeMessage,getuserProject,
-  getAboutus,getProductdata};
+  return axios.post(baseURL + "/message", { params: obj });
+};
+export {
+  getCompanyData,
+  getCompanyDetail,
+  getConnetData,
+  takeMessage,
+  getuserProject,
+  getAboutus,
+  getProductdata
+};
